585b5ee29b7e093a0ca056a5ea711c96f51235ef
Karunpat Promvisut <jomcootle@gmail.com>
2020-10-20 17:06:39 +0700

    Create new branch new_feature

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,8 +1,14 @@
 public class GitTest {
     public static void main(String[] args) {
         sayHello();
+        sayGoodBye();
     }
+
     public static void sayHello() {
         System.out.println("Hello");
+    }
+
+    public static void sayGoodBye() {
+        System.out.println("Bye");
     }
 }
